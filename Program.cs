f6e19af741b135f7f4d016c2ab15401a068cb24b
Juan Carlos Palomo Martínez <juan_ca_88@hotmail.com>
2025-08-14 00:08:39 -0600

    Se retira ManagedIdentity

--- Program.cs
+++ Program.cs
@@ -1,33 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
-using Azure.Identity;
-using Azure.Security.KeyVault.Secrets;
 using TestAAIbackend.Data;
 using TestAAIbackend.Repositories;
 using TestAAIbackend.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configurar conexión con Azure Key Vault
-string keyVaultName = builder.Configuration["KeyVaultName"];
-var keyVaultUri = new Uri($"https://{keyVaultName}.vault.azure.net/"); // ponlo en appsettings.json o en Azure App Settings
-
-// 2. Crear cliente de Key Vault usando Managed Identity (RBAC)
-var secretClient = new SecretClient(vaultUri: keyVaultUri, credential: new DefaultAzureCredential());
-
-// Obtiene la cadena de conexión desde Key Vault de manera asíncrona
-KeyVaultSecret secret = await secretClient.GetSecretAsync("DbConnectionString");
-string connectionString = secret.Value;
-
 // Add services to the container.
 
 builder.Services.AddControllers();
 
-// Add DbContext con Managed Identity
+// Add DbContext
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString));
 
-// Inyección de dependencias: repository & services
+// DI: repository & services
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
 
